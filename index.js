31435c51d4bba9322566ffe9f7e889d6bc782159
toel <toelokemwa@gmail.com>
2024-07-15 13:47:02 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// multiply num1 and num 2
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+//generate random integer
+let random = Math.floor(Math.random() * 100) + 1;
+
+//remainder calculation
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+
+//finding highest number
+let max =   Math.max(10, 5, 20, 15, 8);
